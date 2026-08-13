6a63c764721e4f217d5e10c0abf06638f4e4bcf9
Jaap Stronks <jaap@jaapstronks.nl>
2026-08-13 19:28:33 +0000

    refactor: wrap live/follow route dispatchers in withErrorHandler, drop log-and-500 catches (C7d 3/5)

--- server/routes/api/follow/state.js
+++ server/routes/api/follow/state.js
@@ -1,28 +1,20 @@
-import { methodNotAllowed, serveJson, notFound, serverError } from '../../../utils/http.js';
+import { methodNotAllowed, serveJson, notFound } from '../../../utils/http.js';
 import {
   getFollowStateForPresentation,
 } from '../../../storage/live-sessions/index.js';
 import { getPresentationCached } from '../../../storage/presentation-cache.js';
 import { computeAudienceCapabilitiesFromState, followAudienceScope } from './helpers.js';
-import { createLogger } from '../../../utils/logger.js';
-const log = createLogger('state');
 
 export async function handleFollowState({ repoRoot, req, res }, presentationId) {
   if (req.method !== 'GET') return methodNotAllowed(res, ['GET']);
-  try {
-    const state = await getFollowStateForPresentation(followAudienceScope(repoRoot), presentationId);
-    const pres = await getPresentationCached(followAudienceScope(repoRoot), presentationId);
-    if (!pres) {
-      return notFound(res, 'Presentation not found');
-    }
-    serveJson(res, 200, {
-      ...state,
-      capabilities: computeAudienceCapabilitiesFromState(state, pres),
-    });
-    return true;
-  } catch (err) {
-    log.error('[follow/state] Failed to get follow state:', err);
-    serverError(res, 'Failed to load follow state');
-    return true;
+  const state = await getFollowStateForPresentation(followAudienceScope(repoRoot), presentationId);
+  const pres = await getPresentationCached(followAudienceScope(repoRoot), presentationId);
+  if (!pres) {
+    return notFound(res, 'Presentation not found');
   }
+  serveJson(res, 200, {
+    ...state,
+    capabilities: computeAudienceCapabilitiesFromState(state, pres),
+  });
+  return true;
 }
